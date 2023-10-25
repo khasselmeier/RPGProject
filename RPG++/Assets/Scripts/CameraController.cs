bc621ba879203141b8c9823ad3e63fe9c13ca72c
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-10-25 13:15:39 -0700

    Added tilemap, player, enemy, menu scene

---
+++ RPG++/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    void Update()
+    {
+        //does the player exist?
+        if(PlayerController.me != null && !PlayerController.me.dead)
+        {
+            Vector3 targetPos = PlayerController.me.transform.position;
+            targetPos.z = -10;
+
+            transform.position = targetPos;
+        }
+    }
+}
